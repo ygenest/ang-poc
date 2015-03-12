c7e566d42192d5aed36636158f0f0e61d0fcc39d
info2301 <yves.genest@tdassurance.com>
2015-03-12 15:52:21 -0400

    ajout invoice

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,23 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
+        this.qty = 1;
+        this.cost = 2;
+        this.inCurr = 'EUR';
+        this.currencies = ['USD', 'EUR', 'CNY'];
+        this.usdToForeignRates = {
+            USD: 1,
+            EUR: 0.74,
+            CNY: 6.09
+        };
 
+        this.total = function total(outCurr) {
+            return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+        };
+        this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+            return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+        };
+        this.pay = function pay() {
+            window.alert("Thanks!");
+        };
 }]);
